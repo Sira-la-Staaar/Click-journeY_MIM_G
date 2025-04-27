62b6ef4abda500dca8786eedbf8be99e30d3f522
aidaeldana <eldanaaida2607@gmail.com>
2025-04-27 23:24:41 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,1 +1,120 @@
-x
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <link type="text/css" rel="stylesheet" href="stylesheet.css">
+  <title>S'inscrire</title>
+  <style>
+    body {
+      background: url('https://levoyageautrement-ec2.s3.eu-west-3.amazonaws.com/public/img/upload/magazine/country-430/les-iles-de-loos-un-halot-de-lumiere-dans-la-noirceur-de-conakry/img-2020-10-27-04-04-21-1-long---iles-de-loos-%C2%A9%C2%A9vincent-brudo.jpg') no-repeat center center fixed;
+      background-size: cover;
+    }
+  </style>
+</head>
+<body class="Inscription">
+<form action="votre_script_php.php" method="POST">
+  <img src ="logo3.png" alt="logo de The West Agency"/>
+   
+  <h1 class="aida1">S'inscrire</h1>
+  <h2 class="aida3">Bienvenue sur notre site de voyage !</h2>
+  <h2 class="aida3">Veuillez remplir le formulaire ci-dessous pour créer votre compte et rejoindre l'aventure !</h2>
+  
+  <div class="input box">
+    <h2 class="aida2">Civilité</h2>
+    <select name="Civilité" required style="padding: 10px; border: 2px solid #ccc; border-radius: 8px; width: 250px; font-size: 16px; background-color: #f9f9f9; 
+             color: #333; cursor: pointer; transition: border-color 0.3s ease; margin-left: 20%;margin-right: auto; text-align: left;">
+      <option value="">-- Sélectionnez --</option>
+      <option value="M.">Monsieur</option>
+      <option value="Mme">Madame</option>
+      <option value="Autre">Autre</option>
+    </select>
+  </div>
+  
+  <div class="input box">
+    <h2 class="aida2">Nom</h2>
+    <input type="text" class="input-box" placeholder="Votre nom ici">
+  </div>
+  
+  <div class="input box">
+    <h2 class="aida2">Prénom</h2>
+    <input type="text" class="input-box" placeholder="Votre prénom ici">
+  </div>
+  
+  <div class="input box">
+    <h2 class="aida2">Pays de résidence</h2>
+    <select name="pays_residence" required style="padding: 10px; border: 2px solid #ccc; border-radius: 8px; width: 250px; font-size: 16px; background-color: #f9f9f9; 
+             color: #333; cursor: pointer; transition: border-color 0.3s ease; margin-left: 20%;margin-right: auto; text-align: left;">
+
+      <option value="">-- Sélectionnez --</option>
+      <option value="Guinée">Guinée</option>
+      <option value="Maroc">Maroc</option>
+      <option value="Mali">Mali</option>
+      <option value="Sénégal">Sénégal</option>
+      <option value="Cote d'Ivoire">Côte d'Ivoire</option>
+      <option value="Gambie">Gambie</option>
+      <option value="Burkina Faso">Burkina Faso</option>
+      <option value="Togo">Togo</option>
+      <option value="Bénin">Bénin</option>
+      <option value="Guinée Bissau">Guinée Bissau</option>
+      <option value="Liberia">Liberia</option>
+      <option value="Nigeria">Nigeria</option>
+      <option value="Sierra Leone">Sierra Leone</option>
+      <option value="Ghana">Ghana</option>
+      <option value="Niger">Niger</option>
+      <option value="Mauritanie">Mauritanie</option>
+      <option value="Cap-Vert">Cap-Vert</option>
+    </select>
+  </div>
+  
+  <div class="input box">
+    <h2 class="aida2">Date de naissance</h2>
+    <input type="date" name="date_naissance" min="1930-01-01" max="2007-12-31" required style="padding: 10px; border: 2px solid #ccc; border-radius: 8px; width: 250px; font-size: 16px; background-color: #f9f9f9; 
+             color: #333; cursor: pointer; transition: border-color 0.3s ease; margin-left: 20%;margin-right: auto; text-align: left;"/>
+  </div>
+  
+  <div class="input box">
+    <h2 class="aida2">Adresse e-mail</h2>
+    <input type="email" class="input-box" placeholder="Votre adresse e-mail ici">
+  </div>
+  
+  <div class="input box">
+    <h2 class="aida2">Mot de passe</h2>
+    <input type="password" class="input-box" placeholder="Choissisez votre mot de passe"required minlength="8">
+  </div>
+<?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $champs = ['civilite', 'nom', 'prenom', 'pays_residence', 'date_naissance', 'email', 'mot_de_passe'];
+    f ($formulaire_complet) {
+        echo "Formulaire complet, envoi en cours...";
+        if (empty($_POST[$champ])) {
+            echo "Erreur : Le formulaire est incomplet.";
+            $formulaire_complet = false;
+            break; // Arrêter la boucle si un champ est vide
+        }
+    }
+?>
+
+  <div class="input box" style="margin-top: 20px;">
+  <button type="submit" 
+   style="padding: 10px 20px; background-color: #3498db; border: none; border-radius: 8px; color: white; font-size: 16px; cursor: pointer; margin-left: auto; margin-right: 20%; text-align:right;">
+   S'inscrire
+</button>
+  </div>
+  <footer class="footer2">
+    <div class="pied1"><br><img class="pied11" src="logo5.png" alt="logo de The West Agency"/><br><br> * Selon les conditions tarifaires propres à chaque produit et précisées ci-après : <br><br>
+      - Vols : Tarifs TTC par personne et « à partir de », valables à certaines dates, sous réserve de disponibilité et de confirmation de la compagnie aérienne<br><br>
+      - Séjours : Tarifs TTC, hors taxes de séjour, par personne sur base d'une chambre double. Prix « à partir de » valables à certaines dates et sous réserve de disponibilités et de confirmation. Ces tarifs n'incluent pas les suppléments ou options susceptibles de s'appliquer à certaines réservations ou destinations.<br><br>
+      - Week-ends : Tarifs TTC hors taxes de séjours, indiqués par personne « à partir de », valables à certaines dates et sous réserve de disponibilité et de confirmation. Ces tarifs n'incluent pas les suppléments ou options susceptibles de s'appliquer à certaines réservations ou destinations.<br><br>
+      - Locations : Tarifs TTC par logement et par semaine (sauf mention contraire), « à partir de », valables à certaines dates, sous réserve de disponibilité et de confirmation.<br><br>
+      - Hôtels : Tarifs TTC par nuit et par chambre « à partir de », valables à certaines dates, sur la base d'une chambre double, hors taxes de séjour, ajustables en fonction du taux de change, sous réserve de disponibilité et de confirmation.<br><br>
+      - Voiture : Tarifs TTC « à partir de », par jour et pour un petit véhicule, hors suppléments, valables à certaines dates et sous réserve de disponibilité et de confirmation.<br><br>
+      - Croisières : Tarifs TTC par personne « à partir de », hors taxes de séjours, valables à certaines dates et sous réserve de disponibilité.<br><br><br>
+      
+      © 2024-2025 TheWestAgency. Tous droits réservés - société soumise au droit français, inscrite au registre du commerce de Paris, Cergy 95000, dont le siège social est à l’avenue du Parc, immatriculée au Registre des opérateurs de voyages et de séjours auprès d’Atout France sis 79/81 rue de Clichy 75009 Paris, sous le numéro IM099170015, agréée IATA. 
+      Si vous avez soumis une réclamation auprès de notre Service Client, mais que notre réponse ne vous satisfait pas : vous pouvez contacter la Médiation Tourisme et Voyage sur leur site www.mtv.travel ou par voie postale MTV Médiation Tourisme Voyage BP 80 303 75 823 Paris cedex 17. Vous pouvez nous contacter au sujet de votre remboursement dès maintenant via notre Centre d'aide ou contactez nos agents ici.
+       <br> <br>
+      Laissez-nous votre avis ! <br> <br><a href="https://feedback.emplifi.io/?lID=2&rn=131521&vm=1&pID=5&hs1=102214&hs2=102226&uni=1&siteID=1&am=true&referrer=Link&sdfc=355b766d-131521-c733c77b-141a-42d7-be9f-2e52b426622b&source=102226"><button class="pied1"><strong>Laisser un commentaire</strong></button></a></div>
+</footer>
+</body>
+</html>
+
+
