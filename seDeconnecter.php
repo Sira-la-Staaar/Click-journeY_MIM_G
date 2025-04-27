e5783514115dbe2cbb9759f715fb31c458372563
hjrblq <belquasshajar@gmail.com>
2025-04-27 23:20:09 +0200

    Update and rename logout.php to seDeconnecter.php

--- seDeconnecter.php
+++ seDeconnecter.php
@@ -2,6 +2,6 @@
 session_start(); //on doit acceder à la fonction pour pouvoir la supprimer
 session_unset(); // Vide toutes les variables de session(role, usurname...), mais la session existe toujours
 session_destroy(); // Détruit la session
-header("Location: login.php"); //redirige l'utilisateur vers la page de connexion
+header("Location: seConnecter.php"); //redirige l'utilisateur vers la page de connexion
 exit(); //arreter l'execution du script
 ?>
