ea26dd2bb8f4be27738d7f24c1758a06a2ac45f6
hjrblq <belquasshajar@gmail.com>
2025-05-11 18:17:50 +0200

    Update recapitulatif.php

--- recapitulatif.php
+++ recapitulatif.php
@@ -17,7 +17,7 @@ $error_message = "";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="stylesheet.css">
     <title>Récapitulatif du voyage</title>
 </head>
 <body class="recapitulatif">
