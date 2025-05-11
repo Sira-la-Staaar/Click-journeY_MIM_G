27265dba174353345335faf6e4cfedb2b9d507c1
Sira-la-Staaar <siradiakite792@gmail.com>
2025-05-11 04:26:29 +0200

    Update Accueil.php

--- Accueil.php
+++ Accueil.php
@@ -26,23 +26,25 @@ $voyages = json_decode($voyages_json, true);
           </div>
     </div>
     <div class="section_connect">
-          <?php
-          session_start();
-          if (isset($_SESSION["connecte"]) && $_SESSION["connecte"] === true): ?>
-            <a href="profil.php">
-                <img class="section_connect" src="Images/<?= $_SESSION['utilisateur']['img'] ?>" 
-                alt="<?= htmlspecialchars($_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom']) ?>">
-            </a>
-            <a href="profil.php" class="nom_profil">
-                <?= $_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom']; ?>
-            </a>
-            <?php else: ?>
-              <li>
-                <a href="inscription.php">S'inscrire</a>
-                <a href="seConnecter.php">/Se connecter</a>
-              </li>
-          <?php endif; ?>
-    </div>      
+      <?php
+        session_start();
+        if (isset($_SESSION["connecte"]) && $_SESSION["connecte"] === true):
+          $lien_profil = ($_SESSION['utilisateur']['role'] === 'A') ? 'PageAdmin.php' : 'profil.php';
+          ?>
+          <a href="<?= $lien_profil ?>">
+            <img class="section_connect" src="Images/<?= $_SESSION['utilisateur']['img'] ?>" 
+            alt="<?= htmlspecialchars($_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom']) ?>">
+          </a>
+          <a href="<?= $lien_profil ?>" class="nom_profil">
+            <?= $_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom']; ?>
+          </a>
+        <?php else: ?>
+          <li>
+            <a href="inscription.php">S'inscrire</a>
+            <a href="seConnecter.php">/Se connecter</a>
+          </li>
+        <?php endif; ?>
+    </div>
 
     <form method="GET" action="recherche.php" class="barre-recherche">
       <input type="text" name="q" placeholder="Rechercher un voyage..." required>
