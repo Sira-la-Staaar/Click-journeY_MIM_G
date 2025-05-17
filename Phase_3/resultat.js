fc35bc690ec4a9456b5219d4ece29814c13febf2
hjrblq <belquasshajar@gmail.com>
2025-05-17 22:34:17 +0200

    Update resultat.js

--- Phase_3/resultat.js
+++ Phase_3/resultat.js
@@ -1,4 +1,11 @@
-var villes = ["Marrakech", "Paris", "Dakar"];
+var villes = [ 
+    "Marrakech", "Casablanca", "Rabat", "Conakry", "Kankan", "Bamako", "Mopti", "Kayes",
+    "Paris-Orly", "Paris-CDG", "Paris", "Dakar", "Ziguinchor", "Thiès", "Saint-Louis",
+    "Monrovia", "Buchanan", "Bissau", "Bafata", "Freetown", "Bo", "Cotonou", "Parakou",
+    "Lomé", "Niamtougou", "Accra", "Kumasi", "Lagos", "Abuja", "Abidjan", "Yamoussoukro",
+    "Ouagadougou", "Bobo-Dioulasso", "Banjul", "Farafenni", "Praia", "Mindelo",
+    "Niamey", "Agadez", "Nouakchott", "Nouadhibou"
+];
 
 for (var i = 0; i < villes.length; i++) {
   console.log("Ville : " + villes[i]);
