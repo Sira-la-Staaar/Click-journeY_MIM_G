5c9accbe146257ff28083308bd74f31d49e2c5b9
hjrblq <belquasshajar@gmail.com>
2025-05-17 22:48:10 +0200

    Update Vols.php

--- Phase_3/Vols.php
+++ Phase_3/Vols.php
@@ -10,7 +10,7 @@ session_start();
     		<link rel="shortcut icon" type="image/png" href="Images/minilogo.png"/>
     		<meta charset="UTF-8" />
     		<meta name=”description” content="Vols" />
-		<script src="Vols.js" defer></script>
+		<script src="Vols.js" defer></script> // avec "defer" le navigateur attend que la page HTML soit prête, puis execute le JavaScript sinon le navigateur execute le JavaScript tout de suite, avant d’avoir fini de charger le HTML.
 	</head>
 	<body class="Vols" style="background: url('https://media.istockphoto.com/id/1057333524/fr/photo/kasbah-ait-ben-haddou-dans-le-d%C3%A9sert-pr%C3%A8s-de-montagnes-de-latlas-maroc.jpg?s=612x612&w=0&k=20&c=Zcr5WKI1URXHrsYqBFdydkW0EywJdJXRqKyb4MBzqjc=') no-repeat center center fixed; background-size: cover;">
     <form action="resultat.php" method="GET"> 
