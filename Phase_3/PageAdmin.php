82b5b6a18ac01c3a9f6b5f919bef7bc6ed90528d
Sira-la-Staaar <siradiakite792@gmail.com>
2025-05-18 01:00:47 +0200

    Update PageAdmin.php

--- Phase_3/PageAdmin.php
+++ Phase_3/PageAdmin.php
@@ -24,7 +24,7 @@ $utilisateursPage = array_slice($utilisateurs, $debut, $utilisateursParPage);
 <html>
   <head lang="fr">
     <title>Page Admin | The West Agency</title>
-    <link type="text/css" rel="stylesheet" href="stylesheet.css">
+    <link type="text/css" rel="stylesheet" href="CSS/theme-clair.css">
     <link rel="shortcut icon" type="image/png" href="Images/minilogo.png"/>
     <meta charset="UTF-8" name="author" content="Sira DIAKITE" />
     <meta name="description" content="page admin" />
@@ -33,42 +33,7 @@ $utilisateursPage = array_slice($utilisateurs, $debut, $utilisateursParPage);
     <!--<div class="container">
     <div class="div1"></div>
     <div class="div2">-->
-    <div id="header">        
-        <div id="navbar"> 
-            <ul class="ulButton">
-              <a href="Vols.php"><li class="liButton">Vols</li></a>
-              <a href="Apropos.php"><li class="liButton">A propos de nous</li></a>
-              <a href="Apropos.php#avis-contact-wrapper"><li class="liButton">Contacts</li></a>
-            </ul>
-            <a id="Logo" href="Accueil.php"><img class="disp" src="Images/logo.png" alt="Rechercher"/></a>
-          </div>
-    </div>
-        <!--</div>
-        <div class="div3"></div>
-        </div>-->
-    <div class="section_connect">
-      <?php
-        if (isset($_SESSION["connecte"]) && $_SESSION["connecte"] === true):
-          $lien_profil = ($_SESSION['utilisateur']['role'] === 'A') ? 'PageAdmin.php' : 'profil.php';
-          ?>
-          <a href="<?= $lien_profil ?>">
-            <img class="section_connect" src="Images/<?= $_SESSION['utilisateur']['img'] ?>" 
-            alt="<?= htmlspecialchars($_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom']) ?>">
-          </a>
-          <a href="<?= $lien_profil ?>" class="nom_profil">
-            <?= $_SESSION['utilisateur']['prenom'] . ' ' . $_SESSION['utilisateur']['nom']; ?>
-          </a>
-        <?php else: ?>
-          <li>
-            <a href="inscription.php">S'inscrire</a>
-            <a href="seConnecter.php">/Se connecter</a>
-          </li>
-        <?php endif; ?>
-    </div>
-
-    <form method="GET" action="recherche.php" class="barre-recherche">
-      <input type="text" name="q" placeholder="Rechercher un voyage..." required>
-    </form>
+    <?php include 'header.php'; ?>
 
     <h1 id="admin">Liste des utilisateurs</h1>
 
@@ -90,7 +55,9 @@ $utilisateursPage = array_slice($utilisateurs, $debut, $utilisateursParPage);
                     <td>
                         <form method="post" action="scripts/modifier_utilisateur.php" style="display:inline;">
                             <input type="hidden" name="id" value="<?= $u['id'] ?>">
-                            <input type="submit" name="action" value="Mettre Admin">
+                            <?php if ($u['role'] !== 'A') { ?>
+                              <input type="submit" name="action" value="Mettre Admin">
+                            <?php } ?>
                             <input type="submit" name="action" value="Bannir">
                         </form>
                         
@@ -100,7 +67,9 @@ $utilisateursPage = array_slice($utilisateurs, $debut, $utilisateursParPage);
             <?php endforeach; ?>
         </tbody>
     </table>
-    <a href="seDeconnecter.php"><button id="logout">Se déconnecter</button><a>
+    <a href="seDeconnecter.php"><button class="btn-details" id="logout">Se déconnecter</button><a>
+    <a href="profil.php"><button class="btn-details">Profil</button><a>
+
     <div class="pagination">
         <?php if ($pageActuelle > 1): ?>
             <a href="?page=<?= $pageActuelle - 1 ?>">Précédent</a>
@@ -117,4 +86,3 @@ $utilisateursPage = array_slice($utilisateurs, $debut, $utilisateursParPage);
     <?php include 'footer.php'; ?>
   </body>
 </html>
-
