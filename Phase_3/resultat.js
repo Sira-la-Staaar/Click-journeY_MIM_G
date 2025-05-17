9b6af7005bd89c45b41775349f0a2fae76f1c6a2
hjrblq <belquasshajar@gmail.com>
2025-05-17 22:37:41 +0200

    Update resultat.js

--- Phase_3/resultat.js
+++ Phase_3/resultat.js
@@ -1,4 +1,4 @@
-var villes = [ 
+ var villes = [ //declarer une variable qui est un tableau, ce tableau contient des villes qu'on va utiliser plus tard.
     "Marrakech", "Casablanca", "Rabat", "Conakry", "Kankan", "Bamako", "Mopti", "Kayes",
     "Paris-Orly", "Paris-CDG", "Paris", "Dakar", "Ziguinchor", "Thiès", "Saint-Louis",
     "Monrovia", "Buchanan", "Bissau", "Bafata", "Freetown", "Bo", "Cotonou", "Parakou",
@@ -6,7 +6,7 @@ var villes = [
     "Ouagadougou", "Bobo-Dioulasso", "Banjul", "Farafenni", "Praia", "Mindelo",
     "Niamey", "Agadez", "Nouakchott", "Nouadhibou"
 ];
-
+// tant que i est inférieur à la taille du tableau, on continue, et on affichera toutes les villes!
 for (var i = 0; i < villes.length; i++) {
   console.log("Ville : " + villes[i]);
 }
