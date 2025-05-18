352549dbfa1c86f1e46d065612d457cc4db5c5d5
Sira-la-Staaar <siradiakite792@gmail.com>
2025-05-18 18:32:42 +0200

    Update panier.php

--- Phase_3/panier.php
+++ Phase_3/panier.php
@@ -31,8 +31,8 @@ $total_global = 0;
 <html lang="fr">
 <head>
     <title>Panier | The West Agency</title>
-    <link type="text/css" rel="stylesheet" href="CSS/theme-clair.css">
-    <link rel="shortcut icon" type="image/png" href="Images/minilogo.png" id="theme"/>
+    <link type="text/css" rel="stylesheet" href="CSS/theme-clair.css" id="theme">
+    <link rel="shortcut icon" type="image/png" href="Images/minilogo.png" />
     <meta charset="UTF-8" name="author" content="Sira DIAKITE" />
     <meta name=”description” content="Ensemble de vos sélections" />
 </head>
