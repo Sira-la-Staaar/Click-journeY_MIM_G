0b323905a910592c2cd66cb5666362714b3537b1
aidaeldana <eldanaaida2607@gmail.com>
2025-05-18 20:08:33 +0200

    Update Vols.php

--- Phase_3/Vols.php
+++ Phase_3/Vols.php
@@ -25,18 +25,17 @@ if ($voyages === null) {
 	<body id="Vols">
     <?php include 'header.php'; ?>
       <form action="resultat.php" method="post" id="form-vol">
-        <div class="encadre">
         <h1 class="aida1">-------------Vols-------------</h1>
 </div>
-        <div class="encadre1">
-          <br><br>
-        <p class="aida3">D'ou partez-vous?</p>
+        <p>D'ou partez-vous?</p>
           <select class="btn" name="ville_depart" id="ville_depart">
             <optgroup label="Maroc">
               <option value="Marrakech">Marrakech</option>
               <option value="Casablanca">Casablanca</option>
               <option value="Rabat">Rabat</option>
             </optgroup>
+	
+            </optgroup>
             <optgroup label="Guinee">
               <option value="Conakry">Conakry</option>
               <option value="Kankan">Kankan</option>
@@ -109,14 +108,15 @@ if ($voyages === null) {
               <option value="Nouadhibou">Nouadhibou</option>
             </optgroup>
           </select>
-<br><br>
-        <p class="aida3">D'ou allez-vous?</p>
+         <p>D'ou allez-vous?</p>
           <select class="btn" name="ville_arrivee" id="ville_arrivee">
             <optgroup label="Maroc">
               <option value="Marrakech">Marrakech</option>
               <option value="Casablanca">Casablanca</option>
               <option value="Rabat">Rabat</option>
             </optgroup>
+	
+            </optgroup>
             <optgroup label="Guinee">
               <option value="Conakry">Conakry</option>
               <option value="Kankan">Kankan</option>
@@ -189,21 +189,21 @@ if ($voyages === null) {
               <option value="Nouadhibou">Nouadhibou</option>
             </optgroup>
           </select>
-          <br><br>
-          <p class="aida3">Choisissez votre formule:</p>
+          
+          <p>Choisissez votre formule:</p>
           <select class="btn" name="formule">
             <option value="economique">economique</option>
             <option value="affaire">affaire</option>
             <option value="premiere">premiere</option>
           </select>
-          <br><br>
-          <p class="aida3">Type de voyage:</p>
+         
+          <p>Type de voyage:</p>
           <select class="btn" name="type-voyage" onchange="afficherChamps()">
             <option value="aller-retour">aller-retour</option>
             <option value="aller simple">aller simple</option>
           </select>
-          <br><br>
-          <p class="aida3">Choisissez la date du vol:</p> 
+          
+          <p>Choisissez la date du vol:</p> 
           <?php $today = date('Y-m-d');     // ex. 2025-05-18?>
           <div id="date-aller">
             <label for="aller" class="sol">Date aller :</label>
@@ -214,7 +214,7 @@ if ($voyages === null) {
             <label for="retour" class="sol">Date retour :</label>
             <input type="date" class="btn" id="retour" name="date_retour" min="<?= $today ?>" max="2025-12-31">
           </div>
-          <br><br>
+         
           <label for="voyageurs">Nombre de voyageurs :</label>
           <input type="number" id="voyageurs" name="voyageurs" min="1" max="10" value="1">
           <p>-À noter: <span id="valises">chaque voyageur bénéficie de l'inclusion de deux valises de 23 kg chacune!</span></p>
