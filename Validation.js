a95f3cde6f917b2635847e5496b0eeae80881e4a
hjrblq <belquasshajar@gmail.com>
2025-05-18 02:01:45 +0200

    Update Validation.js

--- Validation.js
+++ Validation.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  if (!form) return;
+document.addEventListener("DOMContentLoaded", function () { // Quand le HTML est chargé, faites function(), tout ce qu’il y a à l’intérieur se lancera une fois que la page est prête.
+  const form = document.querySelector("form"); //chercher le premier élément "form" de la page.
+  if (!form) return; //si le formulaire n’existe pas, on arrete le script
 
-  const emailInput = form.querySelector('input[type="email"]');
-  const passwordInput = form.querySelector('input[type="password"]');
-  const emailError = document.createElement("p");
-  const passwordError = document.createElement("p");
-  const counter = document.createElement("p");
-  const togglePassword = document.createElement("span");
+  const emailInput = form.querySelector('input[type="email"]'); //chercher "email" dans le formulaire
+  const passwordInput = form.querySelector('input[type="password"]'); //chercher "password" dans le formulaire
+  const emailError = document.createElement("p"); // on cree des nv elements qui n'existent pas dans le html! le "p" sert à afficher un message d'erreur pour l'e-mail
+  const passwordError = document.createElement("p"); //erreur pour le mot de passe,
+  const counter = document.createElement("p"); //compter et afficher le nombre de caractères tapés dans le mot de passe :
+  const togglePassword = document.createElement("span"); //pour afficher ou demasquer les mdp! le "span" est utilisée pour ajouter une icône ou un petit texte à côté d’autre chose.
 
-  // Mise en forme
-  emailError.style.color = "red";
-  passwordError.style.color = "red";
-  counter.style.color = "gray";
-  togglePassword.textContent = "👁️";
-  togglePassword.style.cursor = "pointer";
-  togglePassword.style.marginLeft = "10px";
+  // Mise en forme, CSS en ligne ou style en JS.
+  emailError.style.color = "red"; //const emailError = document.createElement("p");
+  passwordError.style.color = "red"; //const passwordError = document.createElement("p");
+  counter.style.color = "green"; //const counter = document.createElement("p");
+  togglePassword.textContent = "👁️"; // const togglePassword = document.createElement("span"); le contenu texte ici c imoji
+  togglePassword.style.cursor = "pointer"; // .cursor : changer le style du curseur de la souris quand on passe sur l’œil, "pointer" ca devient une main pour que l'utilisateur puisse comprendre qu'il peut cliquer dessus!
+  togglePassword.style.marginLeft = "10px"; //ajoute un espace à gauche: 10px, entre le champ mot de passe et l’icône 👁️!
 
-  // Ajout dynamique
+  // Ajout dynamique, 
   if (emailInput) emailInput.parentNode.appendChild(emailError);
   if (passwordInput) {
     passwordInput.parentNode.appendChild(passwordError);
